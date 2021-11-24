f7237e88d9d8256550e20952e6b84bd20e13d369
Rob Evans <robert.evansii@gmail.com>
2021-11-24 14:44:59 -0500

    Create FooterImage.swift

---
+++ HomeRepairNavigator/HelperViews/FooterImage.swift
@@ -0,0 +1,51 @@
+//
+//  FooterImage.swift
+//  HomeRepairNavigator
+//
+//  Created by robevans on 11/24/21.
+//
+
+import SwiftUI
+
+struct FooterImage: View {
+    @Environment(\.openURL) var openURL
+
+    let text: String
+    let image: String
+    var body: some View {
+        VStack {
+            Button(action: {
+
+                switch text {
+                case "Amazon":
+                    openURL(URL(string: "https://www.amazon.com/dp/B00SM5GDV0/ref=dp-kindle-redirect?_encoding=UTF8&btkr=1")!)
+                case "Donate":
+                    openURL(URL(string: "https://www.paypal.com/fundraiser/charity/1381672")!)
+                case "Visit":
+                    openURL(URL(string: "https://naahrf.org/")!)
+                default:
+                    HomeRepairNavigator.actionSheet()
+                }
+
+            }) {
+                ZStack {
+                    Color.gray
+                        .opacity(0.4)
+                        .frame(width: 65, height: 65, alignment: .center)
+                        .clipShape(RoundedRectangle(cornerRadius: 13))
+                    Image(systemName: image)
+                        .font(.title)
+                }
+            }
+            Text(text)
+                .fontWeight(.semibold)
+        }
+    }
+}
+
+
+struct FooterImage_Previews: PreviewProvider {
+    static var previews: some View {
+        FooterImage(text: "Bell", image: "bell")
+    }
+}
